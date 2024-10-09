31302d358456683831459d35331a03945e945d39
YootTanA <oatyootna@gmail.com>
2024-10-09 17:53:55 +0700

    add example unit test

--- products/src/services/product-service.js
+++ products/src/services/product-service.js
@@ -3,71 +3,66 @@ const { FormateData } = require("../utils");
 
 // All Business logic will be here
 class ProductService {
+  constructor() {
+    this.repository = new ProductRepository();
+  }
 
-    constructor(){
-        this.repository = new ProductRepository();
+  async CreateProduct(productInputs) {
+    if (productInputs == "" || productInputs == undefined) {
+      throw new Error("product inputs should not be not defined");
     }
-    
-
-    async CreateProduct(productInputs){
-
-        const productResult = await this.repository.CreateProduct(productInputs)
-        return FormateData(productResult);
-    }
-    
-    async GetProducts(){
-        const products = await this.repository.Products();
-
-        let categories = {};
-
-        products.map(({ type }) => {
-            categories[type] = type;
-        });
-        
-        return FormateData({
-            products,
-            categories:  Object.keys(categories)  
-           })
-
+    if (productInputs.name == "" || productInputs.name == undefined) {
+      throw new Error("product name should not be empty");
     }
 
-    async GetProductDescription(productId){
-        
-        const product = await this.repository.FindById(productId);
-        return FormateData(product)
+    const productResult = await this.repository.CreateProduct(productInputs);
+    return FormateData(productResult);
+  }
+
+  async GetProducts() {
+    const products = await this.repository.Products();
+
+    let categories = {};
+
+    products.map(({ type }) => {
+      categories[type] = type;
+    });
+
+    return FormateData({
+      products,
+      categories: Object.keys(categories),
+    });
+  }
+
+  async GetProductDescription(productId) {
+    const product = await this.repository.FindById(productId);
+    return FormateData(product);
+  }
+
+  async GetProductsByCategory(category) {
+    const products = await this.repository.FindByCategory(category);
+    return FormateData(products);
+  }
+
+  async GetSelectedProducts(selectedIds) {
+    const products = await this.repository.FindSelectedProducts(selectedIds);
+    return FormateData(products);
+  }
+
+  async GetProductPayload(userId, { productId, qty }, event) {
+    const product = await this.repository.FindById(productId);
+
+    if (product) {
+      const payload = {
+        event: event,
+        data: { userId, product, qty },
+      };
+
+      return FormateData(payload);
+    } else {
+      return FormateData({ error: "No product Available" });
     }
-
-    async GetProductsByCategory(category){
-
-        const products = await this.repository.FindByCategory(category);
-        return FormateData(products)
-
-    }
-
-    async GetSelectedProducts(selectedIds){
-        
-        const products = await this.repository.FindSelectedProducts(selectedIds);
-        return FormateData(products);
-    }
-
-    async GetProductPayload(userId,{ productId, qty },event){
-
-         const product = await this.repository.FindById(productId);
-
-        if(product){
-             const payload = { 
-                event: event,
-                data: { userId, product, qty}
-            };
- 
-             return FormateData(payload)
-        }else{
-            return FormateData({error: 'No product Available'});
-        }
-
-    }
- 
-
+  }
 }
 
 module.exports = ProductService;
--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,10 +1,61 @@
-// which service it is
-describe("ProductService", () => {
-  // Which function
-  describe("CreateProduct", () => {
-    // Which Scenario we are testing
-    test("validate user inputs", () => {});
+const { ProductRepository } = require("../database");
+const ProductService = require("./product-service");
 
-    test("Validate response", async () => {});
+describe("ProductService", () => {
+  const productService = new ProductService();
+
+  describe("CreateProduct", () => {
+    const mockProductInputs = {
+      name: "Tiramisu Cake",
+    };
+
+    class mockProductRepository extends ProductRepository {
+      constructor(products) {
+        super();
+        this.products = products;
+      }
+
+      async CreateProduct({
+        name,
+        desc,
+        type,
+        unit,
+        price,
+        available,
+        suplier,
+        banner,
+      }) {
+        const product = {
+          name: name,
+          desc: desc,
+          type: type,
+          unit: unit,
+          price: price,
+          available: available,
+          suplier: suplier,
+          banner: banner,
+        };
+        this.products.push(product);
+
+        return product;
+      }
+
+      async Products() {
+        return this.products;
+      }
+    }
+
+    productService.repository = new mockProductRepository([]);
+
+    test("giving empty product name go as expected", () => {
+      expect(
+        async () => await productService.CreateProduct(),
+      ).rejects.toThrow();
+    });
+
+    test("giving non empty product inputs go as expected", async () => {
+      const result = await productService.CreateProduct(mockProductInputs);
+      expect(result).not.toBeNull();
+    });
   });
 });
