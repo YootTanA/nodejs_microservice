f81f11bf6b0d818ff68f1e7c24519877b4aa6306
YootTanA <oatyootna@gmail.com>
2024-10-10 10:26:46 +0700

    Init test

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,6 +1,37 @@
+const ProductService = require("./product-service");
+const { ProductRepository } = require("../database");
+
 describe("ProductService", () => {
   describe("CreateProduct", () => {
-    test("giving empty product name go as expected", () => {});
+    class mockProductRepository extends ProductRepository {
+      async CreateProduct({
+        name,
+        desc,
+        type,
+        unit,
+        price,
+        available,
+        suplier,
+        banner,
+      }) {
+        return {};
+      }
+    }
+    const productService = new ProductService(mockProductRepository);
+    const productInput = {
+      name: "",
+      desc: "",
+      type: "",
+      unit: "",
+      price: "",
+      available: "",
+      suplier: "",
+      banner: "",
+    };
+
+    test("giving empty product name go as expected", () => {
+      expect(productService.CreateProduct(productInput)).rejects.toThrow();
+    });
 
     test("giving non empty product inputs go as expected", async () => {});
   });
