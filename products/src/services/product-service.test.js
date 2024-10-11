2002285fb303e50a790b9509c2ccbed31fe02908
YootTanA <oatyootna@gmail.com>
2024-10-11 12:15:57 +0700

    Add GetTotalProduct price  test

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,26 +1,35 @@
 const ProductService = require("./product-service");
 const { ProductRepository } = require("../database");
 
-describe("ProductService", () => {
-  class MockProductRepository extends ProductRepository {
-    async CreateProduct({
-      name,
-      desc,
-      type,
-      unit,
-      price,
-      available,
-      suplier,
-      banner,
-    }) {
-      return {};
-    }
+class MockProductRepository extends ProductRepository {
+  constructor(initProducts) {
+    super();
+    this.products = initProducts;
   }
 
-  const mockProductRepository = new MockProductRepository();
-  const productService = new ProductService(mockProductRepository);
+  async CreateProduct({
+    name,
+    desc,
+    type,
+    unit,
+    price,
+    available,
+    suplier,
+    banner,
+  }) {
+    return {};
+  }
 
+  async Products() {
+    return this.products;
+  }
+}
+
+describe("ProductService", () => {
   describe("CreateProduct", () => {
+    const mockProductRepository = new MockProductRepository([]);
+    const productService = new ProductService(mockProductRepository);
+
     test("giving empty product name go as expected", () => {
       const productInputs = {
         name: "",
@@ -47,6 +56,39 @@ describe("ProductService", () => {
         banner: "",
       };
       expect(productService.CreateProduct(productInputs)).not.toBeNull();
+    });
+  });
+
+  describe("GetTotalProductPrice", () => {
+    const products = [
+      {
+        name: "a",
+        desc: "",
+        type: "",
+        unit: "",
+        price: 100,
+        available: "",
+        suplier: "",
+        banner: "",
+      },
+      {
+        name: "b",
+        desc: "",
+        type: "",
+        unit: "",
+        price: 200,
+        available: "",
+        suplier: "",
+        banner: "",
+      },
+    ];
+
+    const mockProductRepository = new MockProductRepository(products);
+    const productService = new ProductService(mockProductRepository);
+
+    test("having 2 products, product a has 100 values, product b has 200 values should return 300", async () => {
+      const productTotalPrice = await productService.GetTotalProductPrice();
+      expect(productTotalPrice).toBe(300);
     });
   });
 });
