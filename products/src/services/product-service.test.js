4b2be1b1de7207b72e1dfe39d272b5afa044875f
YootTanA <oatyootna@gmail.com>
2024-10-11 12:00:59 +0700

    Push Unit Test

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,7 +1,52 @@
-describe("ProductService", () => {
-  describe("CreateProduct", () => {
-    test("giving empty product name go as expected", () => {});
+const ProductService = require("./product-service");
+const { ProductRepository } = require("../database");
 
-    test("giving non empty product inputs go as expected", async () => {});
+describe("ProductService", () => {
+  class MockProductRepository extends ProductRepository {
+    async CreateProduct({
+      name,
+      desc,
+      type,
+      unit,
+      price,
+      available,
+      suplier,
+      banner,
+    }) {
+      return {};
+    }
+  }
+
+  const mockProductRepository = new MockProductRepository();
+  const productService = new ProductService(mockProductRepository);
+
+  describe("CreateProduct", () => {
+    test("giving empty product name go as expected", () => {
+      const productInputs = {
+        name: "",
+        desc: "",
+        type: "",
+        unit: "",
+        price: 100,
+        available: "",
+        suplier: "",
+        banner: "",
+      };
+      expect(productService.CreateProduct(productInputs)).rejects.toThrow();
+    });
+
+    test("giving non empty product inputs go as expected", async () => {
+      const productInputs = {
+        name: "mock",
+        desc: "",
+        type: "",
+        unit: "",
+        price: 100,
+        available: "",
+        suplier: "",
+        banner: "",
+      };
+      expect(productService.CreateProduct(productInputs)).not.toBeNull();
+    });
   });
 });
